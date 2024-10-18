199f65dac77678953259563191a9a5e31d77dbf9
Link753 <119621532+Link753@users.noreply.github.com>
2024-10-18 11:51:29 +0100

    AI Can shoot balls

--- Assets/Scripts/AIScript.cs
+++ Assets/Scripts/AIScript.cs
@@ -8,6 +8,7 @@ public class AIScript : MonoBehaviour
     [Header("CONFIG")]
     [SerializeField] private Transform Player;
     [SerializeField] NavMeshAgent Nav;
+    [SerializeField] int BulletCount;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,18 +19,22 @@ public class AIScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Draw Line from player to object
-        //extend line until out of range
-        //go to end of line
         transform.LookAt(Player);
-        if(Vector3.Distance(transform.position, Player.transform.position) < 25)
+        if(Vector3.Distance(transform.position, Player.transform.position) < 10)
         {
-            Debug.Log("current player pos: " + Player.transform.position);
-            Nav.SetDestination(transform.position-transform.forward);
+            Nav.SetDestination(transform.position - transform.forward);
         }
         else
         {
-            Nav.SetDestination(transform.position);
+            Nav.SetDestination(transform.position + transform.forward);
+        }
+
+        if (BulletCount > 0) 
+        {
+            GameObject bullet = GameManager.instance.GetPooledObject();
+            bullet.SetActive(true);
+            bullet.transform.position = transform.position;
+            bullet.GetComponent<Rigidbody>().AddForce(transform.position + transform.forward);
         }
     }
 }
--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class BallScript : MonoBehaviour
 {
     [SerializeField] float DespawnTime;
-    float passedTime;
+    float passedTime, CollisionIgnoreTime;
     // Start is called before the first frame update
     void OnEnable()
     {
         passedTime = 0;
+        GetComponent<Collider>().enabled = false;
+    }
+
+    private void Awake()
+    {
+        CollisionIgnoreTime = 0.2f;
     }
 
     // Update is called once per frame
     void Update()
     {
         passedTime += Time.deltaTime;
+        if(passedTime > CollisionIgnoreTime)
+        {
+            GetComponent<Collider>().enabled = true;
+        }
         if (passedTime > DespawnTime) 
         {
             passedTime = 0;
